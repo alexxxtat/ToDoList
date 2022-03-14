cd8477639f98a4477685a1502d9009bb61d393bc
Alex Chou <alexchtat@gmail.com>
2022-03-14 16:04:28 -0700

    add local storage functionality

--- app.js
+++ app.js
@@ -45,6 +45,15 @@ add.addEventListener("click", (e) => {
   deleteButton.addEventListener("click", (e) => {
     let todoElement = e.target.parentElement;
     todoElement.addEventListener("animationend", (e) => {
+      //remove from local storage
+      let text = todoElement.children[0].innerText;
+      let myListArray = JSON.parse(localStorage.getItem("list"));
+      myListArray.forEach((item, index) => {
+        if (item.toDoText == text) {
+          myListArray.splice(index, 1);
+          localStorage.setItem("list", JSON.stringify(myListArray));
+        }
+      });
       todoElement.remove();
     });
     todoElement.style.animation = "scaleDown 0.5s forwards";
@@ -53,7 +62,74 @@ add.addEventListener("click", (e) => {
   toDo.appendChild(checkButton);
   toDo.appendChild(deleteButton);
 
+  //create an object
+  let myToDo = {
+    toDoText: form.children[0].value,
+    toDoMonth: form.children[1].value,
+    toDoDay: form.children[2].value,
+  };
+  // store data using local storage\
+  //using an array of objects
+  let myList = localStorage.getItem("list");
+  if (myList == null) {
+    localStorage.setItem("list", JSON.stringify([]));
+  } else {
+    let myListArray = JSON.parse(myList);
+    myListArray.push(myToDo);
+    localStorage.setItem("list", JSON.stringify(myListArray));
+  }
+
   toDo.style.animation = "scaleUp 0.5s forwards";
   form.children[0].value = "";
   session.appendChild(toDo);
 });
+
+let myList = localStorage.getItem("list");
+if (myList !== null) {
+  let myListArray = JSON.parse(myList);
+  myListArray.forEach((item) => {
+    let todo = document.createElement("div");
+    todo.classList.add("todo");
+    let text = document.createElement("p");
+    text.classList.add("todo-text");
+    text.innerText = item.toDoText;
+    let time = document.createElement("p");
+    time.classList.add("todo-time");
+    time.innerText = item.toDoMonth + " / " + item.toDoDay;
+    todo.appendChild(text);
+    todo.appendChild(time);
+
+    let checkButton = document.createElement("button");
+    checkButton.classList.add("check");
+    checkButton.innerHTML = '<i class="fa-solid fa-check-to-slot"></i>';
+    checkButton.addEventListener("click", (e) => {
+      let todoItem = e.target.parentElement;
+      console.log(todoItem);
+      todoItem.classList.toggle("done");
+    });
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete");
+    deleteButton.innerHTML = '<i class="fa-solid fa-delete-left"></i>';
+    deleteButton.addEventListener("click", (e) => {
+      let todoElement = e.target.parentElement;
+      todoElement.addEventListener("animationend", (e) => {
+        let text = todoElement.children[0].innerText;
+        console.log(text);
+        let myListArray = JSON.parse(localStorage.getItem("list"));
+        myListArray.forEach((item, index) => {
+          console.log(item.toDoText);
+
+          if (item.toDoText == text) {
+            myListArray.splice(index, 1);
+            localStorage.setItem("list", JSON.stringify(myListArray));
+          }
+        });
+        todoElement.remove();
+      });
+      todoElement.style.animation = "scaleDown 0.5s forwards";
+    });
+    todo.appendChild(checkButton);
+    todo.appendChild(deleteButton);
+    session.append(todo);
+  });
+}
